7f083c0e132f65cfd9b06123037cbac71155d09c
shawon2911 <hosenuzzaman2018@gmail.com>
2026-02-24 16:09:11 +0600

    struggle part partially added and interview part problem fixed

--- script.js
+++ script.js
@@ -44,34 +44,73 @@ function toggleStyle(id){
     const selected = document.getElementById(id);
     selected.classList.remove('bg-white', 'text-[#64748B]');
     selected.classList.add('bg-blue-500', 'text-white');
+
+    if(id == "interview-tab"){
+        allCards.classList.add('hidden');
+        filteredSection.classList.remove('hidden');
+
+    }
+    else if (id == "all-tab"){
+        allCards.classList.remove('hidden');
+        filteredSection.classList.add('hidden');
+    }
 }
 
-// event handling on main container
-    mainContainer.addEventListener('click', function(event){
-        if(event.target.classList.contains('interview-btn')){
-            const parentNode = event.target.parentNode.parentNode
+// event handling on main container 
+    mainContainer.addEventListener('click', function(event){                                        
+        if(event.target.classList.contains('interview-btn')){                                                   //for interview button
+            const card = event.target.closest('.card-left');
             // catch everything inside a parentNode
-            const companyName = parentNode.querySelector('.company-name').innerText;
-            const position = parentNode.querySelector('.position').innerText;
-            const salary = parentNode.querySelector('.salary').innerText;
-            const statusElement = parentNode.querySelector('.job-status');
-            const responsibility = parentNode.querySelector('.job-responsible').innerText;
+            const companyName = card.querySelector('.company-name').innerText;
+            const position = card.querySelector('.position').innerText;
+            const salary = card.querySelector('.salary').innerText;
+            const status = card.querySelector('.job-status').innerText;
+            const responsibility = card.querySelector('.job-responsible').innerText;
             
             // create a object using these information
             const jobInfo = {
                 companyName,
                 position,
                 salary,
-                status,
+                status :"Interview",
                 responsibility
             }
             // searching that a item is unique or not
             const jobExist = interviewList.find(item => item.companyName == jobInfo.companyName);
             if(!jobExist){
                 interviewList.push(jobInfo);
-                statusElement.innerText = "Interview";
+                card.querySelector('.job-status').innerText = "Interview";
+            
             }
             renderInterview();
+            calculateNumber();
+        }
+        else if(event.target.classList.contains('rejected-btn')){                                                           //for  rejected button
+            const card = event.target.closest('.card-left');
+            // catch everything inside a parentNode
+            const companyName = card.querySelector('.company-name').innerText;
+            const position = card.querySelector('.position').innerText;
+            const salary = card.querySelector('.salary').innerText;
+            const status = card.querySelector('.job-status').innerText;
+            const responsibility = card.querySelector('.job-responsible').innerText;
+            
+            // create a object using these information
+            const jobInfo = {
+                companyName,
+                position,
+                salary,
+                status :"Reject",
+                responsibility
+            }
+            // searching that a item is unique or not
+            const jobExist = rejectedList.find(item => item.companyName == jobInfo.companyName);
+            if(!jobExist){
+                rejectedList.push(jobInfo);
+                card.querySelector('.job-status').innerText = "Reject";
+            
+            }
+            renderRejected();
+            calculateNumber();
             }
         
     })
@@ -87,14 +126,14 @@ function toggleStyle(id){
             div.innerHTML = `
                     <div>
                         <div>
-                            <h3 class="company-name font-semibold text-[18px] text-[#002C5C]">Mobile First Corp</h3>
-                            <p class="position text-[#64748B] ">React Native Developer</p>
-                            <p class="salary text-[14px] text-[#64748B] py-[15px]">Remote • Full-time • $130,000 - $175,000</p>
+                            <h3 class="company-name font-semibold text-[18px] text-[#002C5C]">${interview.companyName}</h3>
+                            <p class="position text-[#64748B] ">${interview.position}</p>
+                            <p class="salary text-[14px] text-[#64748B] py-[15px]">${interview.salary}</p>
                         </div>
                         <div class=" bg-[#EEF4FF] w-[150px] text-center py-[11px] ">
-                            <h4 class="   job-status font-medium text-[14px] text-[#002C5C]  ">NOT APPLIED</h4>
+                            <h4 class="   job-status font-medium text-[14px] text-[#002C5C]  ">${interview.status}</h4>
                         </div>
-                        <p class="job-responsible text-[14px] text-[#323B49] pt-[13px] pb-[30px]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                        <p class="job-responsible text-[14px] text-[#323B49] pt-[13px] pb-[30px]">${interview.responsibility}</p>
 
                         <div class="space-x-2">
                             <button class="btn btn-outline btn-success">INTERVIEW</button>
@@ -110,5 +149,42 @@ function toggleStyle(id){
                         </button>
                     </div>
             `
+            filteredSection.appendChild(div);
+        }
+    }
+    // this function is created for show the rejection on the screen
+    function renderRejected(){
+        filteredSection.innerHTML = '';
+        for(let reject of rejectedList){
+            
+            let div = document.createElement('div')
+            div.className = "card-left bg-white rounded-md p-6 flex justify-between items-start mb-5";
+            div.innerHTML = `
+                    <div>
+                        <div>
+                            <h3 class="company-name font-semibold text-[18px] text-[#002C5C]">${reject.companyName}</h3>
+                            <p class="position text-[#64748B] ">${reject.position}</p>
+                            <p class="salary text-[14px] text-[#64748B] py-[15px]">${reject.salary}</p>
+                        </div>
+                        <div class=" bg-[#EEF4FF] w-[150px] text-center py-[11px] ">
+                            <h4 class="   job-status font-medium text-[14px] text-[#002C5C]  ">${reject.status}</h4>
+                        </div>
+                        <p class="job-responsible text-[14px] text-[#323B49] pt-[13px] pb-[30px]">${reject.responsibility}</p>
+
+                        <div class="space-x-2">
+                            <button class="btn btn-outline btn-success">INTERVIEW</button>
+                            <button class="btn btn-outline btn-error">REJECTED</button>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    <div class="btn right-content p-2 border border-gray-400 rounded-full cursor-pointer mt-2">
+                        <button class="delete-btn cursor-pointer" >
+                            <i class="fa-solid fa-trash-can "></i>
+                        </button>
+                    </div>
+            `
+            filteredSection.appendChild(div);
         }
     }
